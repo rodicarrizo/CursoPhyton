7e0276fdefeaa61bd52664b36b9a7450433ac65d
Rodi <rodicarrizo@gmail.com>
2020-06-27 17:18:57 -0300

    codigoFlask

---
+++ codigoflask/holamundo.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+		return 'hola mundo'
+
+
+@app.route('/params')
+def params():
+	param = request.args.get('params1','no contiene este parametro')
+	return 'El parametro es : {}'.format(param)
+
+
+if __name__ == '__main__':
+	app.run(debug = True, port=8000)
+	
